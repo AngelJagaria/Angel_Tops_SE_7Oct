c95bbfa1740860ece4af72b026ba05473dad4812
AngelJagaria <angeljagaria2556@gmail.com>
2022-10-14 15:57:58 +0530

    day2

---
+++ c programing/classwork/day2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+printf("\t\t\twelcome to c programing\n ");
+printf("\t\tFirst name : Angel\n");
+printf("\t\tLast name : jagaria\n");
+	printf("\t\tage : 16\n");
+printf("\t\tEmail:angeljagaria2556@gmail.com\n");
+}
