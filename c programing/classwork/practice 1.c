bb4447bef3c0d76767eafa328ac77be301248eaf
AngelJagaria <angeljagaria2556@gmail.com>
2022-10-17 22:41:22 +0530

    hw

---
+++ c programing/classwork/practice 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a= 20;
+	int b= 30;
+	printf("\t\t\t\taddition:%d\n",(a+b));
+	printf("\t\t\t\tsub:%d\n",(a-b));
+	printf("\t\t\t\tmulti:%d\n",(a*b));
+	printf("\t\t\t\tdevide:%f\n",(a/(float)b));
+}
