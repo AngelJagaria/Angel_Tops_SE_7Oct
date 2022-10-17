723d8d1ac3de9d73d65258984d842905fc630d79
AngelJagaria <angeljagaria2556@gmail.com>
2022-10-17 17:04:34 +0530

    day3

---
+++ c programing/classwork/day3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a=9;
+	int b=5;
+	printf("\t\t\t\t\tsubstraction:%d\n",(a-b));
+	printf("\t\t\t\t\tmulti:%d\n",(a*b));
+	printf("\t\t\t\t\tdivide:%f\n",(a/(floats)b));
+}
