bb9e0e23ccbab21648cea9864db149103ba6fad6
AngelJagaria <angeljagaria2556@gmail.com>
2022-11-16 16:28:57 +0530

    day10

---
+++ c programing/classwork/day9loop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	//initialization
+	//int i=1;
+	//condition
+	
+	/*
+	while(i<=100)
+	{
+		printf("%d\n",i);
+		i++;//updation
+	}
+	*/
+	int i=100;
+	while(i>=1)
+	{
+		printf("%d\n",i);
+		i--;
+	}
+	
+}
