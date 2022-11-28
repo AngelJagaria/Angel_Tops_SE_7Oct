81a78d141bf51d1759d51b88f87210cb38e00981
AngelJagaria <angeljagaria2556@gmail.com>
2022-11-28 16:44:22 +0530

    name.cpp

---
+++ cpp/classwork/name.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name ="Angel";
+	int age= 16;
+	string address =" 71/c sbk society";
+	int phoneno= 123456789;
+	
+	cout<<" \nYour name is :"<<name;
+	cout<<"\nYour age is:"<<age;
+	cout<<"\nYour address:"<<address;
+	cout<<"\nYour phone no.:"<<phoneno;
+	return 0;
+}
